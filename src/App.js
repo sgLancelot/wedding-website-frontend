5a3ca70b914598bc362ba1c496b72cec0b4c3dbc
tzhenkai <sglancelot@gmail.com>
2020-03-04 22:09:33 +0800

    Changed favicon.ico, added semantic segment, added save the date and date.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react'
-import { Header, Container, Icon, Menu, Image, Button } from 'semantic-ui-react'
+import { Header, Container, Menu, Image, Segment } from 'semantic-ui-react'
 
 function App() {
   return (
-    <Container textAlign='center'>
-      <br/>
-      <Header as='h1' color='black'>Zhen Kai & Pei Wen</Header>
-      <br/>
-      <Menu text size='large' widths={4}>
-        <Menu.Item name='WELCOME'/>
-        <Menu.Item name='STORY'/>
-        <Menu.Item name='SCHEDULE'/>
-        <Menu.Item name='LOCATION'/>
-      </Menu>
-      <Image src='https://zk-wedding-website-images.s3-ap-southeast-1.amazonaws.com/IMG_3058.jpg' size='huge' centered/>
-      <br/>
-      <Header color='pink' as='h2'>
-        10.10.2020
-      </Header>
-      <br/>
-      <Button basic color='black'>
-        RSVP Here!
-      </Button>
-    </Container>
+    <Segment vertical>
+      <Container text textAlign='center' style={{marginTop: '2em'}}>
+        <Menu text size='large' widths={3}>
+            <Menu.Item name='WEDDING'/>
+            <Menu.Item name='RSVP'/>
+            <Menu.Item name='PHOTOS'/>
+        </Menu>
+        <Header size='huge' style={{marginTop: '2em'}}>Pei Wen & Zhen Kai</Header>
+        <Image src='https://zk-wedding-website-images.s3-ap-southeast-1.amazonaws.com/IMG_3058.jpg' size='huge' style={{marginTop: '3em'}}/>
+        <Container text style={{marginTop: '3em'}}>
+          <Header size='large' style={{margin: '2em 0em 2em'}}>10 - 10 - 2020</Header>
+          <p>Stay tuned!</p>
+        </Container>
+      </Container>
+    </Segment>
   )
 }
 
