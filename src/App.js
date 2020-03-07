bbf601c13780a15fde1baa55dc0fb3fa12e692e2
tzhenkai <sglancelot@gmail.com>
2020-03-07 12:06:15 +0800

    Tested grid successfully. Will be working on transiting to a full grid layout.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Header, Container, Menu, Image, Segment } from 'semantic-ui-react'
+import { Header, Container, Menu, Image, Segment, Icon, Form, Button, Grid } from 'semantic-ui-react'
 
 const MainPage = () => {return(
   <>
-    <Image src='https://zk-wedding-website-images.s3-ap-southeast-1.amazonaws.com/IMG_3058.jpg' size='huge' style={{marginTop: '3em'}}/>
+    <Image src='https://zk-wedding-website-images.s3-ap-southeast-1.amazonaws.com/IMG_3058.jpg' size='huge'/>
     <Container text style={{marginTop: '3em'}}>
       <Header size='large' style={{margin: '2em 0em 1em'}}>10 - 10 - 2020</Header>
       <p>Stay tuned!</p>
@@ -12,22 +12,70 @@ const MainPage = () => {return(
   </>
 )}
 
-const Wedding = () => <p>WEDDDINGGGGGGGG</p>
-const RSVP = () => <p>RSVPPPPPP</p>
-const Photos = () => <p>PICSSSSSSSSSSS</p>
+const Wedding = () => {
+  return (
+    <Segment placeholder>
+      <Header icon>
+        <Icon name='attention' />
+        Work in progress
+      </Header>
+    </Segment>
+  )
+}
+
+const RSVP = ({
+  name,
+  contact,
+  additional,
+  handleNameChange,
+  handleContactChange,
+  handleAdditionalChange,
+  handleAddGuest
+}) => (
+  <Form onSubmit={handleAddGuest}>
+    <Form.Field>
+      <label>Name</label>
+      <input placeholder='Name' type='text' value={name} onChange={handleNameChange} />
+    </Form.Field>
+    <Form.Field>
+      <label>Contact Number</label>
+      <input placeholder='Contact Number' value={contact} onChange={handleContactChange} />
+    </Form.Field>
+    <Form.Field>
+      <label>Additional guests</label>
+      <input placeholder='Additional guests' value={additional} onChange={handleAdditionalChange} />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
+
+const Photos = () => {
+  return (
+    <Segment placeholder>
+      <Header icon>
+        <Icon name='attention' />
+        Work in progress
+      </Header>
+    </Segment>
+  )
+}
 
 function App() {
   return (
     <>
     <Router>
       <Segment vertical>
-        <Container text textAlign='center' style={{margin: '1em'}}>
+        <Container celled text textAlign='center' style={{margin: '1em'}}>
           <Menu text size='large' widths='3'>
             <Menu.Item as={Link} to="/wedding">WEDDING</Menu.Item>
             <Menu.Item as={Link} to="/rsvp">RSVP</Menu.Item>
             <Menu.Item as={Link} to="/photos">PHOTOS</Menu.Item>
           </Menu>
-          <Header as={Link} exact="true" to="/" size='huge'>Pei Wen & Zhen Kai</Header>
+          <Grid centered columns={1} padded='vertically'>
+            <Grid.Column textAlign='center' style={{margin: '1em 0em 3em'}}>
+             <Header as={Link} exact="true" to="/" size='huge' margins={4}>Pei Wen & Zhen Kai</Header>
+            </Grid.Column>
+          </Grid>
           <Route exact path="/" render={() => <MainPage />} />
           <Route path="/wedding" render={() => <Wedding />} />
           <Route path="/rsvp" render={() => <RSVP />} />
